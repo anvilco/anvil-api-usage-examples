f850c16360d797c4e24f0a908d6e184762c9866a
Allan Almazan <allan.almazan@gmail.com>
2022-08-16 21:52:05 -0700

    Base example

---
+++ csharp/examples/FillPdf.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+using Anvil.Client;
+using AnvilExamples;
+using Newtonsoft.Json.Linq;
+
+class FillPDF : RunnableBaseExample
+{
+    static async Task<JObject> CallCurrentUserQuery(GraphQLClient client)
+    {
+        var query = @"query CurrentUser {
+            currentUser {
+                eid
+                name
+                organizations {
+                    eid
+                    slug
+                    name
+                    casts {
+                        eid
+                        name
+                    }
+                    welds {
+                        eid
+                        name
+                    }
+                }
+            }
+        }";
+        return await client.SendQuery(query, null);
+    }
+
+    static async Task<JObject> CallWeldQuery(GraphQLClient client, string weldEid)
+    {
+        var query = @"
+            query WeldQuery (
+                $eid: String,
+            ) {
+                weld (
+                    eid: $eid,
+                ) {
+                    eid
+                    name
+                    forges {
+                        eid
+                        slug
+                        name
+                    }
+                }
+            }";
+        return await client.SendQuery(query, new {eid = weldEid});
+    }
+
+    public FillPDF(string apiKey) : base(apiKey)
+    {
+        Console.WriteLine(this._apiKey);
+    }
+
+    override public async Task Run(string apiKey)
+    {
+        Console.WriteLine("Fill pdf");
+    }
+}
---
+++ csharp/examples/RunnableBaseExample.cs
@@ -0,0 +1,21 @@
+namespace AnvilExamples;
+
+public interface IBaseExample
+{
+    Task Run(string apiKey);
+}
+
+abstract class RunnableBaseExample : IBaseExample
+{
+    protected string _apiKey;
+
+    public RunnableBaseExample(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public virtual Task Run(string apiKey)
+    {
+        throw new NotImplementedException();
+    }
+}
